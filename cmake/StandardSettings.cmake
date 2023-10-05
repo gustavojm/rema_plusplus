55ba49514337a57a5834ec5f144af579ee526e5c
Gustavo Malano <gmalano@na-sa.com.ar>
2023-10-05 12:48:13 -0300

    compila pero no linkea

---
+++ cmake/StandardSettings.cmake
@@ -0,0 +1,24 @@
+#
+# Project settings
+#
+option(${PROJECT_NAME}_BUILD_EXECUTABLE "Build the project as an executable, rather than a library." ON)
+option(${PROJECT_NAME}_BUILD_HEADERS_ONLY "Build the project as a header-only library." OFF)
+option(${PROJECT_NAME}_USE_ALT_NAMES "Use alternative names for the project, such as naming the include directory all lowercase." OFF)
+
+#
+# Compiler options
+#
+
+option(${PROJECT_NAME}_WARNINGS_AS_ERRORS "Treat compiler warnings as errors." ON)
+
+#
+# Miscelanious options
+#
+
+# Generate compile_commands.json for clang based tools
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
+option(${PROJECT_NAME}_VERBOSE_OUTPUT "Enable verbose output, allowing for a better understanding of each step taken." ON)
+option(${PROJECT_NAME}_GENERATE_EXPORT_HEADER "Create a `project_export.h` file containing all exported symbols." OFF)
+
+
