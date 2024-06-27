816f759d8e1261888f391b61740dc0bdfc52897f
Gustavo Malano <gmalano@na-sa.com.ar>
2024-06-27 11:41:38 -0300

    added reentrant system calls and improved the non reentrant ones
    *define pvHeapEnd on linker script to detect when heap tends to overwrite stack

--- newlib/syscalls.c
+++ newlib/syscalls.c
@@ -1,6 +1,10 @@
 #include <errno.h>
 #include <signal.h>
 #include <sys/reent.h>
+#include <malloc.h>
+
+#include "FreeRTOS.h"
+#include "task.h"
 
 #define WEAK __attribute__((__used__, weak))
 #define WEAK_INIT __attribute__((__used__, weak, constructor))
@@ -60,17 +64,74 @@ USED int _open(const char *name, int f, int m) {
 
 #endif
 
-void *_sbrk(uint32_t delta) {
-  extern char _pvHeapStart; /* Defined by the linker */
-  static char *heap_end;
-  char *prev_heap_end;
-  if (heap_end == 0) {
-    heap_end = &_pvHeapStart;
-  }
-  prev_heap_end = heap_end;
-  // if (prev_heap_end+delta > get_stack_pointer()) {
-  //       return (void *) -1L;
-  // }
-  heap_end += delta;
-  return (void *)prev_heap_end;
+//      These values come from the linker file.
+//
+extern uint8_t _pvHeapStart;  // located at the first byte of heap
+extern uint8_t _pvHeapEnd;    // located at the first byte after the heap
+
+static uint8_t* currentHeapPos = &_pvHeapStart;
+
+// newlib_malloc_helpers
+
+//      The application may choose to override this function.  Note that
+// after this function runs, the malloc-failed hook in FreeRTOS is likely
+// to be called too.  It won't be called, however, if we're here due to a
+// "direct" call to malloc(), not via pvPortMalloc(), such as a call made
+// from inside newlib.
+//
+WEAK void sbrkFailedHook( ptrdiff_t size )
+{
+   configASSERT(0);
+}
+
+void *_sbrk(ptrdiff_t size) {
+  void* returnValue;
+  
+  if (currentHeapPos + size > &_pvHeapEnd) {
+      sbrkFailedHook(size);
+      returnValue = (void*)-1;
+   } else {
+      returnValue = (void*)currentHeapPos;
+      currentHeapPos += size;
+   }
+   
+   return (returnValue);
+}
+
+
+//      The implementation of _sbrk_r() included in some builds of newlib
+// doesn't enforce a limit in heap size.  This implementation does.
+//
+void* _sbrk_r(struct _reent *reent, ptrdiff_t size) {
+   void* returnValue;
+
+   if (currentHeapPos + size > &_pvHeapEnd) {
+      sbrkFailedHook(size);
+
+      reent->_errno = ENOMEM;
+      returnValue = (void*)-1;
+   } else {
+      returnValue = (void*)currentHeapPos;
+      currentHeapPos += size;
+   }
+
+   return (returnValue);
+}
+
+//      In the pre-emptive multitasking environment provided by FreeRTOS,
+// it's possible (though unlikely) that a call from newlib code to the
+// malloc family might be preempted by a higher priority task that then
+// makes a call to the malloc family.  These implementations of newlib's
+// hook functions protect against that.  Most calls to this function come
+// via pvPortMalloc(), which has already suspended the scheduler, so the
+// call here is redundant most of the time.  That's OK.
+//
+void __malloc_lock(struct _reent *r)
+{
+   vTaskSuspendAll();
+}
+
+void __malloc_unlock(struct _reent *r)
+{
+   xTaskResumeAll();
 }
