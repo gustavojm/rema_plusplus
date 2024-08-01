e342cb20be6066a610d6571a3b4475fc37ad57b0
Gustavo Malano <gmalano@na-sa.com.ar>
2024-08-01 16:16:29 -0300

    changed Doxygen generation

--- cmake/Doxygen.cmake
+++ cmake/Doxygen.cmake
@@ -5,7 +5,7 @@ if(${PROJECT_NAME}_ENABLE_DOXYGEN)
     set(DOXYGEN_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/docs)
 
     find_package(Doxygen REQUIRED dot)
-    doxygen_add_docs(doxygen-docs ${SOURCES} ${HEADERS})
+    doxygen_add_docs(doxygen-docs ${HEADERS} ${SOURCES})
 
     verbose_message("Doxygen has been setup and documentation is now available. Generate using `doxygen-docs` target (ie: cmake --build build --target doxygen-docs)")
 endif()
