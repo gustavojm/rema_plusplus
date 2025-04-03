dd2218bdd2aaff6283a1b9cc262f420c31878b22
Gustavo Malano <gmalano@na-sa.com.ar>
2025-04-03 10:32:15 -0300

    flash to CIAA now working fine

--- flash_CIAA.sh
+++ flash_CIAA.sh
@@ -1,3 +1,3 @@
-openocd -c "gdb_port 50000" -c "tcl_port 50004" -c "telnet_port 50008" -f ./openOCD/lpc4337.cfg -c "program ./build/REMA_plusplus.bin verify reset exit"
+openocd -c "gdb_port 50000" -c "tcl_port 50004" -c "telnet_port 50008" -f ./openOCD/lpc4337.cfg -c "init" -c "halt 0" -c "flash write_image erase ./build/REMA_plusplus.bin 0x1A000000 bin" -c "reset run" -c "shutdown" 2>&1
 
-bash
+bash -c 'read -n 1 -s -r -p "Press any key to continue"'
